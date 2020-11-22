a97b2808e0da5ed08ed266cdbb18db67f07da93b
Matthew Robinson <matthewrobinson@Matthews-MBP.local>
2020-11-22 01:49:17 -0700

    imports

---
+++ knn.py
@@ -0,0 +1,15 @@
+# Standard powerful python tools
+import pandas as pd
+import numpy as np
+
+# Used for splitting the data
+from sklearn.model_selection import train_test_split
+# Scalar preprocesser used to deal with bias
+from sklearn.preprocessing import StandardScaler
+# Classifier we're using
+from sklearn.neighbors import KNeighborsClassifier
+# Testing metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+
