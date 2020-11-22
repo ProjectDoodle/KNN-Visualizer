af5e8b135fac48094d5d2a09ecec556fe146a7b9
Matthew Robinson <matthewrobinson@Matthews-MBP.local>
2020-11-22 02:03:13 -0700

    added euclidean function

--- knn.py
+++ knn.py
@@ -1,6 +1,7 @@
 # Standard powerful python tools
 import pandas as pd
 import numpy as np
+import math
 
 # Used for splitting the data
 from sklearn.model_selection import train_test_split
@@ -13,3 +14,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 
+def euclidan_distance(x, y):
+    distance = 0.0
+    for i in range(len(x) - 1):
+        distance += (x[i] - y[i]) ** 2
+    return math.sqrt(distance)
